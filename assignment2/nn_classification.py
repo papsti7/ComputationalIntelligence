c12e04b4486ffd361acbb550d50495bc9496744a
Stefan Papst <stefan.papst@codefluegel.com>
2017-04-12 16:49:37 +0200

    implemented task 2_1 and 2_2

--- assignment2/nn_classification.py
+++ assignment2/nn_classification.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import confusion_matrix, mean_squared_error
 
 from sklearn.neural_network.multilayer_perceptron import MLPClassifier
-from nn_classification_plot import plot_hidden_layer_weights, plot_histogram_of_acc
+from nn_classification_plot import plot_hidden_layer_weights, plot_histogram_of_acc, plot_image
 import numpy as np
 
 
@@ -21,21 +21,47 @@ Fill in all the sections containing TODO!
 
 def ex_2_1(input2, target2):
     ## TODO
-
     classifier = MLPClassifier(
         hidden_layer_sizes=(6,),
         solver="adam",
         max_iter=200,
         activation="tanh"
     )
-    print(input2.shape)
-    print(target2.shape)
-    conf_mat = confusion_matrix(input2, target2)
-    print(conf_mat)
+
+    classifier.fit(input2, target2[:,1])
+    con_mat = confusion_matrix(target2[:,1], classifier.predict(input2))
+    plot_hidden_layer_weights(classifier.coefs_[0])
+
+
     pass
 
 
 def ex_2_2(input1, target1, input2, target2):
     ## TODO
+    scores = []
+    classifiers = []
+    for i in range(10):
+        classifier = MLPClassifier(
+            hidden_layer_sizes=(20,),
+            solver="adam",
+            max_iter=1000,
+            activation="tanh",
+            random_state=i
+        )
+        classifier.fit(input1, target1[:, 0])
+        scores.append(classifier.score(input2, target2[:,0]))
+        classifiers.append(classifier)
+
+    conf_mat = confusion_matrix(target2[:,0], classifiers[np.argmax(scores)].predict(input2))
+
+    plot_histogram_of_acc(classifier.score(input1, target1[:,0]), classifier.score(input2, target2[:,0]))
+    predected_target = classifier.predict(input2)
+    misclassified_images = []
+    for i in range(len(target2[:,0])):
+        if target2[:,0][i] != predected_target[i]:
+            misclassified_images.append(input2[i])
+
+    for i in range(len(misclassified_images)):
+        plot_image(misclassified_images[i])
     pass
 
